95593b559fa0708434230fb32ac1030c049f9518
Thiago Giammattey <giammattey@gmail.com>
2019-03-07 14:24:30 -0300

    Built out Task component and added storyshots test

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,10 @@
 import { configure } from '@storybook/vue';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../stories', true, /.stories.js$/);
+import '../src/index.css';
+
+const req = require.context('../src', true, /.stories.js$/);
 function loadStories() {
-  req.keys().forEach(filename => req(filename));
+	req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
